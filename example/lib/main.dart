81152d7c04e1b139499f10b4b1340ed3c2f93457
Jamie Kerber <kerber.jg@gmail.com>
2025-07-14 03:20:19 +0200

    Added example code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,122 +1,205 @@
 import 'package:flutter/material.dart';
+import 'package:multi_screen_flutter_linux/widgets.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ExampleApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+abstract class _State {
+  final int counter;
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _State(this.counter);
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+class FirstScreenState extends _State {
+  FirstScreenState(super.counter);
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class SecondScreenState extends _State {
+  SecondScreenState(super.counter);
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class ExampleApp extends StatelessWidget {
+  ExampleApp({super.key});
+
+  final ValueNotifier<FirstScreenState> firstCounter =
+      ValueNotifier<FirstScreenState>(FirstScreenState(0));
+  final ValueNotifier<SecondScreenState> secondCounter =
+      ValueNotifier<SecondScreenState>(SecondScreenState(0));
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    return MultiProvider(
+      providers: [
+        ValueListenableProvider.value(value: firstCounter),
+        ValueListenableProvider.value(value: secondCounter),
+      ],
+      child: DualScreenApp(
+        orientation: Axis.vertical,
+        spacing: 10.0,
+        firstScreen: SubscreenData(width: 1024, height: 540),
+        firstScreenBuilder: (context) => Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(title: const Text('First Screen')),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              firstCounter.value = FirstScreenState(
+                firstCounter.value.counter + 1,
+              );
+            },
+            child: const Icon(Icons.add),
+          ),
+          body: Builder(
+            builder: (context) => Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('This is the first screen.'),
+                  Consumer<FirstScreenState>(
+                    builder: (context, state, child) {
+                      return Text('First screen counter: ${state.counter}');
+                    },
+                  ),
+                  Consumer<SecondScreenState>(
+                    builder: (context, state, child) {
+                      return Text('Second screen counter: ${state.counter}');
+                    },
+                  ),
+                  // Popup button
+                  ElevatedButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: const Text('Dialog'),
+                          content: const Text(
+                            'This is a dialog on the first screen.',
+                          ),
+                          actions: [
+                            TextButton(
+                              onPressed: () => Navigator.of(context).pop(),
+                              child: const Text('Close'),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                    child: const Text('Show dialog'),
+                  ),
+                  // Snackbar button
+                  ElevatedButton(
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: const Text(
+                            'This is a snackbar on the first screen.',
+                          ),
+                          action: SnackBarAction(
+                            label: 'Undo',
+                            onPressed: () {
+                              // Handle undo action
+                            },
+                          ),
+                        ),
+                      );
+                    },
+                    child: const Text('Show snackbar'),
+                  ),
+                  // Bottom sheet button
+                  ElevatedButton(
+                    onPressed: () {
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (context) => Container(
+                          padding: const EdgeInsets.all(16.0),
+                          child: const Text(
+                            'This is a bottom sheet on the first screen.',
+                          ),
+                        ),
+                      );
+                    },
+                    child: const Text('Show bottom sheet'),
+                  ),
+                  // Navigation button
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Scaffold(
+                            appBar: AppBar(title: const Text('New Screen')),
+                            body: Center(
+                              child: const Text(
+                                'This is a new screen navigated from the first screen.',
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                    child: const Text('Navigate to new screen'),
+                  ),
+                ],
+              ),
             ),
-          ],
+          ),
+        ),
+        secondScreen: SubscreenData(width: 800, height: 420),
+        secondScreenBuilder: (context) => Scaffold(
+          backgroundColor: Colors.green,
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              secondCounter.value = SecondScreenState(
+                secondCounter.value.counter + 1,
+              );
+            },
+            child: const Icon(Icons.add),
+          ),
+          appBar: AppBar(title: const Text('Second Screen')),
+          body: Builder(
+            builder: (context) => Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('This is the second screen.'),
+                  Consumer<FirstScreenState>(
+                    builder: (context, state, child) {
+                      return Text('First screen counter: ${state.counter}');
+                    },
+                  ),
+                  Consumer<SecondScreenState>(
+                    builder: (context, state, child) {
+                      return Text('Second screen counter: ${state.counter}');
+                    },
+                  ),
+                  // Popup button
+                  ElevatedButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: const Text('Dialog'),
+                          content: const Text(
+                            'This is a dialog on the second screen.',
+                          ),
+                          actions: [
+                            TextButton(
+                              onPressed: () => Navigator.of(context).pop(),
+                              child: const Text('Close'),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                    child: const Text('Show dialog'),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
